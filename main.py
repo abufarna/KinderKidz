c27852694442d530f5cc46a1c4f98d495d5c8a90
abufarna <abufarna@rose-hulman.edu>
2024-03-19 14:30:04 -0400

    main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+# Test Code
+## Noor
+
+# Import string and random module
+import string
+import random
+ 
+# Randomly choose a letter from all the ascii_letters
+randomLetter = random.choice(string.ascii_letters)
+print(randomLetter)
